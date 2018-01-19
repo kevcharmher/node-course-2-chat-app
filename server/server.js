e0296a26429ab6be815ed3d7145945bf9e68f26e
Kevin Her <kevcharmher@Kevins-iMac.local>
2018-01-19 10:25:51 -0800

    Init commit

--- server/server.js
+++ server/server.js
@@ -1,50 +1,13 @@
 const path = require('path');
-const http = require('http');
 const express = require('express');
-const socketIO = require('socket.io');
-const { generateMessage, generateLocationMessage } = require('./utils/message');
-
 const publicPath = path.join(__dirname, '../public');
+
 const port = process.env.PORT || 3000;
 
 var app = express();
-var server = http.createServer(app);
-// Configure server to use socketIO
-//io is our web socket server
-var io = socketIO(server);
 
 app.use(express.static(publicPath));
-// Register event listner
-io.on('connection', socket => {
-  console.log('New User connected');
 
-  socket.emit(
-    'newMessage',
-    generateMessage('Admin', 'Welcome to the chat app')
-  );
-
-  socket.broadcast.emit(
-    'newMessage',
-    generateMessage('Admin', 'New user joined')
-  );
-
-  socket.on('createMessage', (message, callback) => {
-    io.emit('newMessage', generateMessage(message.from, message.text));
-    callback();
-  });
-
-  socket.on('createLocationMessage', coords => {
-    io.emit(
-      'newMessageLocationMessage',
-      generateLocationMessage('Admin', coords.latitude, coords.longitude)
-    );
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected to server');
-  });
-});
-
-server.listen(port, () => {
-  console.log(`Server is up on ${port}`);
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
